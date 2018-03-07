ce5cb4ee8ba92d0c1a65d812f501f3faea1cb7b3
lailaafroz <35794882+lailaafroz@users.noreply.github.com>
2018-03-07 08:28:27 +0600

    server receives msg

---
+++ server1.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+#include<string.h>
+#include<limits.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<errno.h>
+#include<unistd.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<stdlib.h>
+#include<fstream>
+     
+using namespace std;
+
+int main()     
+{
+      int client,server;
+
+      int portNum = 3000;
+      bool isExit = false;
+      int bufSize = 100;
+      char buffer[bufSize];
+
+      struct sockaddr_in  server_addr;
+      //int socketlen_t size;
+     // int size;
+
+      client = socket(AF_INET,SOCK_STREAM,0);
+
+      if(client<0)
+      {
+            cout << "error establishing connection" << endl;
+            exit(1);
+      }
+
+      cout << "server socket connection created " << endl;
+
+      server_addr.sin_family =AF_INET;
+      server_addr.sin_port =  htons(portNum);
+      server_addr.sin_addr.s_addr = htons(INADDR_ANY);
+      //server_addr.sin_zero;
+
+      //BIND SOCKET
+      if(bind(client,(struct sockaddr*)&server_addr,sizeof(server_addr))<0)
+      {
+            cout << "error binding socket " << endl;
+            exit(1);
+      }
+
+      cout << " loking for client " << endl;
+      listen(client,1);
+
+      socklen_t SockAddrSize = sizeof(server_addr);
+      server = accept(client,(struct sockaddr*)&server_addr, &SockAddrSize);
+
+      if(server<0){
+       cout << "error on accepting " << endl;
+        exit(1);
+      }
+           
+      if(server>0)  
+      {   
+            strcpy(buffer,"server connected ....../n");
+            send(server,buffer,bufSize,0);
+                
+            cout<< "connected with client" << endl;
+            cout<< "you can communicate now" << endl;
+      }
+      
+      //string str;   
+      do{
+            ofstream oFile;
+            oFile.open("flower2.jpg",ios::binary);
+            do{  
+                recv(server,buffer,bufSize,0); 
+                  oFile << *buffer;    
+                 cout<< buffer << endl;                  
+            }while(*buffer!=EOF);
+            
+              if(*buffer=='B'){
+               exit(1);
+                isExit =false;
+              }
+          cin >> buffer;
+          send(server,buffer,bufSize,0);        
+        }while(!isExit);
+      close(client);
+      return 0;
+}
