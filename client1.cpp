422478d020ce2039d9522f911793c92cf30ee460
lailaafroz <35794882+lailaafroz@users.noreply.github.com>
2018-05-28 18:07:56 +0600

    Add files via upload
    transfer file

--- client1.cpp
+++ client1.cpp
@@ -11,16 +11,26 @@
 #include <arpa/inet.h>
 #include<stdlib.h>
 #include<netdb.h>
+#include<fstream>
 
 using namespace std;   
 
-int main()
-{   
       int client,server;
       int portNum = 3001;
       bool isExit = true;
       int bufSize = 100;
-      char buffer[bufSize];
+      char buffer[12];
+      ifstream f1;
+      //ofstream f2;
+
+void makeFile()
+{ 
+      f1.open("firstFile.txt",ios::in);
+      //f2.open("secondFile.txt",ios_base::binary)
+}
+
+int main()
+{
       //char *ip = "10.100.109.85";
 
       client = socket(AF_INET,SOCK_STREAM,0);
@@ -28,7 +38,7 @@ int main()
       if(client<0)
       {        
             cout << "Error creating socket...." << endl;
-            exit(1);
+            //exit(1);
       }
 
       cout << "client socket created...." << endl;
@@ -50,25 +60,36 @@ int main()
       {
            // cout  <<  "connecting to server...." << endl;
       }
-      //recv(client,buffer,bufSize,0);
+      recv(client,buffer,bufSize,0);
       cout << buffer << endl;
       cout << "conection confirmed" << endl;
       cout << "you can connect server" << endl;
       cout << "send msg to server" << endl;
+      
+      makeFile();
       do{  
-             cin >> buffer ;
+            int i=0;
+            memset(buffer,'a',12);
+            while(i<10){       
+               f1 >> buffer[i];
+               i++;
+            }
                send(client,buffer,bufSize,0);
-            
-                 recv(client,buffer,bufSize,0);
+            break;
+                 /*recv(client,buffer,bufSize,0);
                   cout<< buffer << endl;
                    if(*buffer=='B'){
-                    isExit=false;
-                   }
-      }while(isExit);
+                    isExit=false;*/
+                   //}
+      }while(true);
+
+      f1.close();
+      //f2.close();
+      // return 0;
 
       cout << "connection terminated" << endl;
       cout << "goodbye" << endl;
-      //close(client);
+      close(client);
 
       return 0;
 }
